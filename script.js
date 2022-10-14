fbc863a93c8f81b93f02948f36bb40cc25fc4ba0
lucas-freitas-guimaraes <freitas0023@gmail.com>
2022-10-14 17:38:43 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let btnLogin = document.getElementById('btn');
 let email = document.getElementById('email');
 let senha = document.getElementById('password');
+let concorda = document.getElementById('agreement');
+let btnForm = document.getElementById('submit-btn');
 
 function login(){
     if(email.value === 'tryber@teste.com' && senha.value === '123456'){
@@ -12,3 +14,14 @@ function login(){
 }
 
 btnLogin.addEventListener('click', login);
+
+function testeCheck(){
+    if(concorda.checked == true){
+        btnForm.disabled = false;
+    }
+    else{
+        btnForm.disabled = true;
+    }
+}
+
+concorda.addEventListener('click', testeCheck);
