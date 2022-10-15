275b112849d4ca8b6bddb0e6a06ae0d156c97e2e
Allan Medeiros Minato <allan.minato@gmail.com>
2022-10-15 18:12:34 -0300

    Resolução do exercicio 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const email = document.getElementById('email');
 const senha = document.getElementById('password');
 const concorda = document.getElementById('agreement');
 const btnForm = document.getElementById('submit-btn');
+const count = document.getElementById('counter');
+const textArea = document.getElementById('textarea');
 
 function login() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -17,9 +19,18 @@ btnLogin.addEventListener('click', login);
 function testeCheck() {
   if (concorda.checked) {
     btnForm.disabled = false;
+    btnForm.style.backgroundColor = 'rgb(88, 31, 153)';
   } else {
     btnForm.disabled = true;
+    btnForm.style.backgroundColor = 'grey';
   }
 }
 
 concorda.addEventListener('click', testeCheck);
+
+count.innerHTML = 500;
+function contador(event) {
+  count.innerHTML = 500;
+  count.innerHTML -= event.target.value.length;
+}
+textArea.addEventListener('keyup', contador);
